375a9dd0d9de12b199e2d85118a82c0f1a6803d9
Kevin McHugh <kevin.mchugh@centro.net>
2014-06-22 16:13:34 -0500

    Stealing more code

--- lib/kevins_propietary_brain.rb
+++ lib/kevins_propietary_brain.rb
@@ -4,6 +4,7 @@ module KevinsPropietaryBrain
 ## All Brains must be put in the PlayerBrain module to allow them to be picked up by the game initalizer
   class Brain
     attr_accessor :player
+
     #The brain is instantiated with it's role.  So that it can be used later in the game
     def initialize(role)
       @role = role
@@ -11,12 +12,31 @@ module KevinsPropietaryBrain
 
     # you have the option of picking from many cards, pick the best one.
     def pick(number, *cards)
-      cards.flatten.first(number)
+      ordered = cards.flatten.map do |card|
+        i = card_preference.map { |preference| card.type == preference }.index(true)
+        {card: card, index: i}
+      end
+      ordered.sort_by { |h| h[:index] || 99 }.first(number).map {|h| h[:card] }
     end
-
+    def card_preference
+      [Card.wells_fargo_card, Card.stage_coach_card, Card.jail_card, Card.barrel_card,
+        Card.beer_card, Card.indians_card, Card.gatling_card, Card.scope_card, Card.mustang_card,
+        Card.missed_card, Card.bang_card, Card.revcarbine_card, Card.remington_card, Card.winchester_card,
+        Card.volcanic_card, Card.panic_card, Card.cat_balou_card]
+    end
     #After instantiation the Game will pass the brain two characters from the characters in the models/characters folder, the choose_character method must return one of the two characters.
     def choose_character(character_1, character_2)
-      character_1
+      (character_preference & [character_1, character_2]).first
+    end
+    def character_preference
+      [:PaulRegretPlayer,:PedroRamirezPlayer,
+      :RoseDoolanPlayer,:KitCarlsonPlayer,
+      :SidKetchumPlayer,:ElGringoPlayer,
+      :VultureSamPlayer,:SuzyLafayettePlayer,
+      :WillyTheKidPlayer,:SlabTheKillerPlayer,
+      :CalamityJanetPlayer,:JourdonnaisPlayer,
+      :LuckyDukePlayer,:BartCassidyPlayer,
+      :JesseJonesPlayer,:BlackJackPlayer]
     end
 
     #This method is called on your brain when you are the target of a card that has a bang action (a missable attack). Your brain is given the card that attacked them.  The method should return a card from your hand
@@ -33,24 +53,83 @@ module KevinsPropietaryBrain
     def target_of_duel(card, targetter)
       player.from_hand(Card.bang_card)
     end
+
     #This method is called if your hand is over the hand limit, it returns the card that you would like to discard.
     # Returning nil or a card you don't have is a very bad idea. Bad things will happen to you.
     def discard
-      player.hand.first
+      non_damage_dealing = player.hand.find {|card| !card.damage_dealing? }
+      non_damage_dealing || player.hand.first
     end
 
     #This is the method that is called on your turn.
     def play
-      bang = player.from_hand(Card.bang_card)
-      if bang
-        left_player = player.players.first
-        if left_player && left_player.distance_to <= 1
-          player.play_card(bang, left_player)
-        end
+      bangs_played = 0
+
+      while !player.hand.find_all(&:draws_cards?).empty?
+        player.hand.find_all(&:draws_cards?).each {|card| player.play_card(card)}
+      end
+      play_guns
+      player.hand.each do |card|
+        target = find_target(card)
+        next if card.type == Card.missed_card || !target
+        next if card.type == Card.jail_card && !target || target.sheriff?
+        next if card.type == Card.bang_card && over_bang_limit(bangs_played)
+        bangs_played += 1 if card.type == Card.bang_card
+        player.play_card(card, target, :hand)
       end
     end
+    def draw_choice(*choices)
+      choices.first
+    end
+    def notify(event); end
 
     private
     attr_reader :role
+
+    def over_bang_limit(n)
+      return false if player.character == "Character::WillyTheKidPlayer"
+      return false if player.in_play.detect{ |x| x.type == Card.volcanic_card }
+      return false if n < 1
+      true
+    end
+
+    def find_target(card)
+      if role == 'sheriff'
+        weakest_player_in_range_of(card)
+      elsif role == 'outlaw'
+        player.players_in_range_of(card).include?(sheriff) ? sheriff : weakest_player_in_range_of(card)
+      elsif role == 'renegade'
+        if player.players.size > 1
+          weakest_non_sheriff_in_range_of(card)
+        else
+          sheriff
+        end
+      elsif role == 'deputy'
+        weakest_non_sheriff_in_range_of(card)
+      end
+    end
+
+    def weakest_non_sheriff_in_range_of(card)
+      players_in_range = player.players_in_range_of(card)
+      weakest_players = players_in_range.sort_by { |player| player.health }
+      weakest_players.find { |player| !player.sheriff? }
+    end
+
+    def weakest_player_in_range_of(card)
+      in_range = player.players_in_range_of(card)
+      in_range.min_by { |p| p.health } || in_range.first
+    end
+
+    def sheriff
+      player.players.find(&:sheriff?)
+    end
+
+    def play_guns
+      guns = player.hand.find_all(&:gun?)
+      return if guns.empty?
+      longest_ranged_gun = guns.max { |gun| gun.range }
+      existing_gun = player.in_play.find(&:gun?)
+      player.play_card(longest_ranged_gun) if !existing_gun || (longest_ranged_gun && longest_ranged_gun.range > existing_gun.range)
+    end
   end
 end
--- lib/kevins_propietary_brain/version.rb
+++ lib/kevins_propietary_brain/version.rb
@@ -1,3 +1,3 @@
 module KevinsPropietaryBrain
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
